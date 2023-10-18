b252fdba73ed613feadf80869d277b65c6ea10db
LocTD <it.tranducloc@gmail.com>
2023-10-18 12:12:35 +0700

    Add keymap to VISUAL search

--- lua/config/keymaps.lua
+++ lua/config/keymaps.lua
@@ -2,3 +2,4 @@
 -- Default keymaps that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/keymaps.lua
 -- Add any additional keymaps here
 vim.keymap.set("n", "yp", '<cmd>let @+ = expand("%")<cr>', { desc = "Copy full path of current buffer" })
+vim.keymap.set("n", "g/", "/\\%V", { desc = "VISUAL search" })
